4a44194d9de81942a2d528cfb5b38de4099fb31d
Unknown <athds24@gmail.com>
2018-02-24 15:05:06 -0500

    Level01
    Player can shoot & move

---
+++ Space Invaders/Assets/missles.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missles : MonoBehaviour {
+
+
+
+    public GameObject drone1;
+    public GameObject drone2;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "enemy")
+        {
+            drone1.SetActive(false);
+        }
+        if (collision.gameObject.tag == "enemy1")
+        {
+            drone2.SetActive(false);
+        }
+
+    }
+}
---
+++ Space Invaders/Assets/playerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+
+    public float speed = 10f;
+    public GameObject leftBullet;
+    public Vector3 TargetPos;
+    bool hitedge;
+	// Use this for initialization
+	void Start () {
+        TargetPos = leftBullet.transform.position;
+
+        hitedge = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckInput();
+
+
+        Checkbounds();
+
+
+        float MoveStep = speed * Time.deltaTime;
+        leftBullet.transform.position = Vector3.MoveTowards(leftBullet.transform.position, TargetPos, MoveStep);
+	}
+
+
+    void CheckInput()
+    {
+         
+        if (Input.GetKey("space")){
+            leftBullet.SetActive(true);
+            TargetPos = transform.position + new Vector3(0f, 9f, 0f);
+            hitedge = false;
+        }
+        else
+        {
+            if(hitedge == true)
+            {
+
+                TargetPos = transform.position;
+
+            }
+            
+        }
+         transform.Translate(Vector3.left * Input.GetAxis("Horizontal") * -speed * Time.deltaTime);
+
+    }
+
+    void Checkbounds()
+    {
+        if(leftBullet.transform.position.y == 5.27f)
+        {
+            leftBullet.transform.position = transform.position;
+
+            hitedge = true;
+
+        }
+    }
+}
